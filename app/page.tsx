c920b435577237c90dfc6d060f47a006288f62f9
kourosh-H <kourosh.hajivand0@gmail.com>
2023-11-11 15:56:13 +0330

    ✅ Update

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,23 @@ import useDraw from "@/util/hook/useDraw";
 
 export default function Home() {
   const { canvesRef } = useDraw();
+  function drawline({ prevPoint, curentPoint, ctx }: Draw) {
+    const { x: currX, y: currY } = curentPoint;
+    const lineColor = "#000";
+    const lineWidth = 5;
+    let startPoint = prevPoint ?? curentPoint;
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = lineColor;
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(currX, currY);
+    ctx.stroke();
+
+    ctx.fillStyle = lineColor;
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
   return (
     <div className="w-full h-screen flex items-center justify-center py-10">
       <canvas
--- util/hook/useDraw.tsx
+++ util/hook/useDraw.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef } from "react";
 
 export default function useDraw() {
   const canvesRef = useRef<HTMLCanvasElement>(null);
+
   useEffect(() => {
     const Handler = (e: MouseEvent) => {
       console.log({ x: e.clientX, y: e.clientY });
